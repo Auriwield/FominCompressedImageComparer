0e575c32de798fc41c0cdb29b835b6eddc2b474a
gzaripov <gzaripov@icloud.com>
2017-09-16 19:12:09 +0300

    implement show file in canvas

--- image-comparer/public/js/main.js
+++ image-comparer/public/js/main.js
@@ -34,6 +34,15 @@ function onSelectOrDragImage() {
 $(document).ready(function () {
     onSelectOrDragImage()
         .then(function (file) {
-            console.log(file);
+            var dropZone = $(".drop-zone:first");
+            var workZone = $(".work-zone:first");
+            dropZone.addClass("hidden");
+            workZone.removeClass("hidden");
+            var url = window.URL.createObjectURL(file);
+            var leftCanvas = $("#left-canvas");
+            var ctx = leftCanvas[0].getContext("2d");
+            var leftImage = new Image();
+            leftImage.src = url;
+            ctx.drawImage(leftImage, 0, 0);
         });
 });
