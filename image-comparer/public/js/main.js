44f50a81400deee7b8753d21cfcfb972281304df
Auriwield <xronx11@gmail.com>
2017-09-16 23:08:48 +0300

    implement resizing image

--- image-comparer/public/js/main.js
+++ image-comparer/public/js/main.js
@@ -38,11 +38,37 @@ $(document).ready(function () {
             var workZone = $(".work-zone:first");
             dropZone.addClass("hidden");
             workZone.removeClass("hidden");
-            var url = window.URL.createObjectURL(file);
-            var leftCanvas = $("#left-canvas");
-            var ctx = leftCanvas[0].getContext("2d");
-            var leftImage = new Image();
-            leftImage.src = url;
-            ctx.drawImage(leftImage, 0, 0);
+            drawCanvas(file);
+            $(window).resize(function () {
+                drawCanvas(file);
+            });
         });
 });
+
+function drawCanvas(file) {
+    var url = window.URL.createObjectURL(file);
+    var leftCanvas = $("#left-canvas");
+    var ctx = leftCanvas[0].getContext("2d");
+    var leftImage = new Image();
+    leftImage.onload = function () {
+        var size = calcCanvasSize(leftImage);
+        leftCanvas[0].width = size.width;
+        leftCanvas[0].height = size.height;
+        ctx.drawImage(leftImage, 0, 0, leftImage.width, leftImage.height,
+            0, 0, size.width, size.height);
+    };
+    leftImage.src = url;
+}
+
+function calcCanvasSize(image) {
+    var maxWidth = $(window).width() / 2 * 0.9;
+    var maxHeight = ($(window).height() - $(".header").height()) * 0.9;
+    var mpX = maxWidth / image.width;
+    var mpY = maxHeight / image.height;
+    var mp = Math.min(mpX, mpY);
+
+    return {
+        width: image.width * mp,
+        height: image.height * mp
+    };
+}
