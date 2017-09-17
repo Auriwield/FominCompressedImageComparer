4ffef5a167369c74ac38f9f84ade22b05d7225a5
Auriwield <xronx11@gmail.com>
2017-09-17 23:39:06 +0300

    implement resizing image in more flexible way (using image data)

--- image-comparer/public/js/bcn/bcn1.js
+++ image-comparer/public/js/bcn/bcn1.js
@@ -1,7 +1,15 @@
 var bc1 = (function() {
 
-    var encode = function(src, w1, h1) {
+    var encode = function(imageData) {
+        var w = imageData.width;
+        var h = imageData.height;
+        var src = imageData.data;
 
+        for (var y = 0; y < h; y+=4) {
+            for (var x = 0; x < w; x+=4) {
+
+            }
+        }
     };
 
     return {
