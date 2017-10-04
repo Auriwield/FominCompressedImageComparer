ed79343c0abbb3933773df87aea62452714350da
Grigory Zaripov <grigory.zaripov@firstlinesoftware.com>
2017-10-04 22:37:35 +0300

    add range bar to diff tab

--- image-comparer/public/js/tabs/diff.js
+++ image-comparer/public/js/tabs/diff.js
@@ -1,26 +1,36 @@
 $("nav li a[href='#diff']").first().parent().click(function () {
 
-    function getDiff(leftImageData, rightImageData) {
+    function getDiff(leftImageData, rightImageData, mp = 1) {
         let dest = new Uint8ClampedArray(leftImageData.data.length);
         for (let i = 0; i < leftImageData.data.length; i += 4) {
             for (let j = i; j < i + 3; j++) {
-                dest[j] = Math.abs(leftImageData.data[j] - rightImageData.data[j])
+                let value = Math.abs(leftImageData.data[j] - rightImageData.data[j]) * mp;
+                dest[j] = value > 255 ? 255 : value;
             }
             dest[i + 3] = 255;
         }
         return dest;
     }
 
-    let diffSrc = getDiff(imageData.left, imageData.right);
-    let diffImageData = new ImageData(diffSrc, imageData.left.width, imageData.left.height);
+    let rangeBar = $("input.diff-bar:first");
+    let lastMp = 1;
+    let lastDiffSrc = getDiff(imageData.left, imageData.right);
 
-    let scale = canvasUtils.calcScale(diffImageData, 1);
-    let canvas = $("#diff-canvas");
-    canvasUtils.drawIntoCanvas(diffImageData, canvas, scale);
+    function showDiff() {
+        let mp = parseFloat(rangeBar.val());
+        let diffSrc;
+        if (mp !== lastMp)
+            diffSrc = getDiff(imageData.left, imageData.right, mp);
+        else
+            diffSrc = lastDiffSrc;
 
-    $(window).resize(function () {
+        let diffImageData = new ImageData(diffSrc, imageData.left.width, imageData.left.height);
         let scale = canvasUtils.calcScale(diffImageData, 1);
         let canvas = $("#diff-canvas");
         canvasUtils.drawIntoCanvas(diffImageData, canvas, scale);
-    });
+    }
+
+    $(window).resize(showDiff);
+    rangeBar.on("input change", showDiff);
+    showDiff();
 });
